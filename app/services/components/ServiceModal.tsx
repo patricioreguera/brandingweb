a107e28a1208718e2b3946533642be8e8d98c3ad
patricioreguera <reguerapatriciohernan@gmail.com>
2023-12-28 16:52:08 +0100

    new modal

--- app/services/components/ServiceModal.tsx
+++ app/services/components/ServiceModal.tsx
@@ -32,7 +32,6 @@ interface ServiceIterface {
 
 export const ServiceModal = ({ service }: ServiceIterface) => {
 	const { isOpen, onOpen, onOpenChange } = useDisclosure();
-	console.log(service);
 
 	return (
 		<>
