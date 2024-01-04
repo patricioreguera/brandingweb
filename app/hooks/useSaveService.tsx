04004efdc382154a72bd3dd5685f41da2bb9cdf0
patricioreguera <reguerapatriciohernan@gmail.com>
2024-01-04 16:44:01 +0100

    Fix: budget error

--- app/hooks/useSaveService.tsx
+++ app/hooks/useSaveService.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { AppContext } from "../context/ContextProvider";
 import confetti from "canvas-confetti";
 import { ServiceInterface } from "../interfaces/ServiceInterface";
@@ -7,9 +6,7 @@ export const useSaveService = ({ service }: ServiceInterface) => {
 	const { services, setServices } = AppContext();
 
 	const isDuplicate = (service: any) =>
-		services.some(
-			(existingService) => existingService?._id === service?._id || service?.id
-		);
+		services.some((existingService) => existingService?._id === service?._id);
 
 	const saveInContext = (item: any) => {
 		let duplicatedResult = isDuplicate(item);
